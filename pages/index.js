d26b98030c70711deeb29848dc2ecf377644213e
bidursapkota00 <bidursapkota00@gmail.com>
2022-01-07 23:02:47 +0545

    minor project

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import { Grid } from '@material-ui/core';
+import Layout from '../components/Layout';
+import db from '../utils/db';
+import Product from '../models/Product';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import { Store } from '../utils/Store';
+import ProductItem from '../components/ProductItem';
+
+export default function Home(props) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const { products } = props;
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <ProductItem
+                product={product}
+                addToCartHandler={addToCartHandler}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}, '-reviews').lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
+}
